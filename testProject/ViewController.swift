08bd71b1fef566cf819950283b152a736d7621a9
kameike <kameike@users.noreply.github.com>
2018-11-10 13:16:16 +0900

    Initial Commit

---
+++ testProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testProject
+//
+//  Created by Kei Kameda on 2018/11/10.
+//  Copyright © 2018年 Kei Kameda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
